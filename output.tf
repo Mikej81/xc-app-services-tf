dec3e534db51fb502938f5902122ac382c902eab
Mikej81 <stumpj@gmail.com>
2023-08-22 14:43:22 -0400

    output update

--- output.tf
+++ output.tf
@@ -1,8 +1,5 @@
 output "deployment_info" {
   value = {
-    create_dns_records = "Please be sure to create the following records: Type: CNAME Address: ${module.xc.lb_cname} OR Type: A Record:${var.delegated_dns_domain} IP Address: ${module.xc.lb_ip_address}"
-    #http_lb_public_address = module.xc.lb_ip_address
-    #lb_dns_address         = module.xc.lb_dns_address
-    #lb_dns_value           = module.xc.lb_dns_value
+    create_dns_records = "Please be sure to create the following records: Type: CNAME:${var.delegated_dns_domain} Address: ${module.xc.lb_cname} \nOR Type: A Record:${var.delegated_dns_domain} IP Address: ${module.xc.lb_ip_address}"
   }
 }
