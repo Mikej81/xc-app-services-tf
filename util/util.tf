6b1be2bccfb8d66cba89c37d6050882c4890c973
Mikej81 <stumpj@gmail.com>
2024-03-05 15:05:27 -0500

    vesctl util mods

--- util/util.tf
+++ util/util.tf
@@ -16,6 +16,18 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
+resource "null_resource" "get_xc_tenant_pubkey" {
+  provisioner "local-exec" {
+    command = "vesctl --p12-bundle ./creds/f5-sa.console.ves.volterra.io.api-creds.p12 request secrets get-public-key > xc-api-pubkey"
+  }
+}
+
+resource "null_resource" "get_xc_tenant_policy" {
+  provisioner "local-exec" {
+    command = "vesctl --p12-bundle ./creds/f5-sa.console.ves.volterra.io.api-creds.p12 request secrets get-policy-document --namespace shared --name ves-io-allow-volterra > xc-api-policy"
+  }
+}
+
 output "env_prefix" {
   value = random_string.env_prefix.result
 }
